34583b1f071c7f4dad2ce575c3514c2f699a763e
Ashish <ashish061104@gmail.com>
2025-02-19 01:04:20 +0530

    react basics

---
+++ project1/src/App.jsx
@@ -0,0 +1,9 @@
+import "./app.css"
+import Product from "./product.jsx"
+function App() {
+  return <>
+  <h1>Hello World <button>Welcome</button></h1>
+  <Product/>
+  </>
+}
+export default App
---
+++ project1/src/product.jsx
@@ -0,0 +1,8 @@
+import "./product.css"
+function Product(){
+    return(
+        <h3 class="top">Hey there</h3>
+    )
+}
+
+export default Product;
