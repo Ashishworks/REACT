41b0323c6c709583979a32f8de4efdd3e57a20f4
Ashish <ashish061104@gmail.com>
2025-02-27 12:37:08 +0530

    react todolist final

--- project2/src/toDoList.jsx
+++ project2/src/toDoList.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid';
 export default function ToDoList() {
-    let [arrItem, setdoitem] = useState([{ task: "sample text", id: uuidv4() }])
+    let [arrItem, setdoitem] = useState([{ task: "sample text", id: uuidv4(), markAsDone: false}])
     let [newToDo, setnewToDo] = useState("")
     let add = () => {
-        setdoitem([...arrItem, { task: newToDo, id: uuidv4() }])
+        setdoitem([...arrItem, { task: newToDo, id: uuidv4(), markAsDone:false}])
         setnewToDo("")
     }
     let updateToDo = (event) => {
@@ -24,6 +24,31 @@ export default function ToDoList() {
             })
         ))
         
+    }
+
+    let markAsDoneAll = () => {
+        setdoitem((previtems) => (
+            previtems.map((previtem) => {
+                return {
+                    ...previtem,markAsDone: true
+                };
+            })
+        ))
+        
+    }
+
+    let markAsDone=(id)=>{
+        setdoitem((previtems)=>
+            previtems.map((previtem)=>{
+                if(previtem.id==id){
+                    return{...previtem, markAsDone: true
+                    };
+                }
+                else{
+                    return previtem;
+                }
+            })
+        )
     }
     let upperCaseOne = (id) => {
         setdoitem((previtems) => 
@@ -49,9 +74,10 @@ export default function ToDoList() {
                 {
                     arrItem.map((todo) => (
                         <li key={todo.id}>
-                            <span>{todo.task}</span> &nbsp;&nbsp;
+                            <span style={todo.markAsDone? {textDecoration: "line-through"} : {} }>{todo.task}</span> &nbsp;&nbsp;
                             <span><button onClick={() => deleteit(todo.id)}>DELETE</button></span> {/* We haven't directly use deleteit() cuz it will execute the function but we also have to pass argument so we ()=> that helps use to write deleteit(todo) without its immediate*/}
-                            <span><button onClick={()=> upperCaseOne(todo.id)}>UPPERCASE ONE</button></span>
+                            <span><button onClick={()=> upperCaseOne(todo.id)}>UPPERCASE ONE</button></span>&nbsp;&nbsp;
+                            <span><button onClick={()=>{markAsDone((todo.id))}}>Mark as Done</button></span>
                             <span><br></br></span>
                             <span><br></br></span>
                         </li>
@@ -59,7 +85,8 @@ export default function ToDoList() {
                     )
                 }
             </ul>
-            <button onClick={() => upperCase()}>UPPERCASE ALL</button>
+            <button onClick={() => upperCase()}>UPPERCASE ALL</button> &nbsp;&nbsp;
+            <span><button onClick={markAsDoneAll}>Mark as Done All</button></span>
         </div>
     )
 }
